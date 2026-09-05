735ca98fe62948a51b5f327542b5655a6b911818
plutyx <myads.tech@gmail.com>
2026-09-04 23:57:34 -0300

    feat: route account recovery links before app bootstrap

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './app'
+import { AccountRoute } from './account-actions'
 import './styles.css'
 import './market.css'
 
+function Root(){
+  const accountRoute=<AccountRoute/>
+  if(accountRoute)return accountRoute
+  const hasSession=Boolean(localStorage.getItem('c360_token'))
+  return <>
+    <App/>
+    {!hasSession&&<a className="account-helper-link" href="/?forgot=1">Esqueci minha senha</a>}
+  </>
+}
+
 createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <Root />
   </React.StrictMode>,
 )
