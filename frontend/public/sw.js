09686acaec2cbdc0b4d2631316b7b0e10b4c4a31
plutyx <myads.tech@gmail.com>
2026-09-07 00:02:05 -0300

    Rotate PWA cache for v5.4

--- frontend/public/sw.js
+++ frontend/public/sw.js
@@ -1,4 +1,4 @@
-const VERSION='c360-shell-v53'
+const VERSION='c360-shell-v54'
 const SHELL=["/","/manifest.webmanifest"]
 self.addEventListener('install',event=>{event.waitUntil(caches.open(VERSION).then(cache=>cache.addAll(SHELL)).then(()=>self.skipWaiting()))})
 self.addEventListener('activate',event=>{event.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(key=>key!==VERSION&&key.startsWith('c360-shell-')).map(key=>caches.delete(key)))).then(()=>self.clients.claim()))})
